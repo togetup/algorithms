6cb28b97f0b998bc32643a4b40bb5c97885c3495
togetup <togetup@yeah.net>
2018-10-02 09:08:33 +0800

    2-4 数组中查询元素和修改元素：Array类的toString方法及验证

--- Data-Structures/Java/Array/src/Array.java
+++ Data-Structures/Java/Array/src/Array.java
@@ -91,4 +91,23 @@ public class Array {
         data[index] = e;
         size++;
     }
+
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array:size = %d, capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1){
+                res.append(", ");
+            }
+        }
+        res.append("]");
+
+        return res.toString();
+    }
+
+
 }
--- Data-Structures/Java/Array/src/Main.java
+++ Data-Structures/Java/Array/src/Main.java
@@ -2,24 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 1. 声明固定长度，并循环赋值
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
         }
-
-        // 2. 声明即赋值
-        int[] scores = new int[]{100, 99, 88};
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println(scores[i]);
-        }
-
-        // 3. 修改数组元素，并使用for-in遍历输出
-        // 能使用 for-in 这种方式是因为arry是可迭代的，涉及到偏底层，后面研究
-        scores[0] = 98;
-        for (int score : scores) {
-            System.out.println(score);
-        }
-
+        System.out.println(arr);
     }
 }
